e55f2ae16b59b3ead14e568481ff8479c789c89c
Xiyu Chen <yochen0302@gmail.com>
2025-02-23 12:12:37 -0800

    chore: test commit

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,8 +6,6 @@ class_name Player
 # The downward acceleration when in the air, in meters per second squared.
 #@export var fall_acceleration: float = 75.0
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
